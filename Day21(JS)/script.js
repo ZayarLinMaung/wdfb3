e245ee46818d332917a460bcc37b81a9b8d5bbe6
Kira-god <kirayuzo189@gmail.com>
2024-03-26 14:39:24 +0630

    first commit

--- Day21(JS)/script.js
+++ Day21(JS)/script.js
@@ -1,13 +1,31 @@
 const addBtn = document.querySelector(".addbtn");
 addBtn.onclick = function () {
-  const task = document.querySelector("input").value;
+  const task = document.querySelector("input").value.trim();
   const listItem = document.createElement("li");
-  listItem.textContent = task;
-  const delbtn = document.createElement("button");
-  delbtn.textContent = "delete task";
-  delbtn.onclick = function () {
-    listItem.remove();
-  };
-  document.querySelector("ul").appendChild(listItem);
-  listItem.appendChild(delbtn);
+  if (task !== "") {
+    listItem.textContent = task;
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "edit";
+    editBtn.onclick = function () {
+      let newTask = prompt("Change the task");
+      listItem.textContent = newTask;
+      listItem.appendChild(editBtn);
+      listItem.appendChild(delBtn);
+    };
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "delete";
+    delBtn.onclick = function () {
+      listItem.remove();
+    };
+    document.querySelector("ul").appendChild(listItem);
+    listItem.appendChild(editBtn);
+    listItem.appendChild(delBtn);
+  }
+  document.querySelector("input").value = "";
+  document.querySelector("input").focus();
+};
+document.querySelector("input").onkeyup = function (event) {
+  if (event.key === "Enter") {
+    addBtn.onclick();
+  }
 };
