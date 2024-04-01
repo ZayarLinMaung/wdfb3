b457eff9b20df9f4e50b93986593e05f0427a9b4
Kira-god <kirayuzo189@gmail.com>
2024-04-01 12:49:30 +0630

    first  commit

---
+++ Day23(VueDisplayToggle)/src/App.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      isShow: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.isShow = true;
+    },
+    hideModal() {
+      this.isShow = false;
+    },
+    toggleModal() {
+      this.isShow = !this.isShow;
+    },
+  },
+};
+</script>
+
+<template>
+  <div v-show="isShow" class="modalbox">
+    Modal
+    <button @click="hideModal">Hide Modal</button>
+  </div>
+  <button @click="showModal">Show Modal</button>
+  <button @click="toggleModal">Toggle</button>
+</template>
+
+<style>
+.modalbox {
+  box-sizing: border-box;
+  width: 500px;
+  height: 500px;
+  border: 1px solid black;
+  padding: 30px;
+}
+</style>
---
+++ Day23(VueIf)/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="login">
+      <input
+        v-model="username"
+        type="text"
+        name=""
+        id=""
+        placeholder="Enter username"
+      />
+      <input
+        v-model="email"
+        type="email"
+        name=""
+        id=""
+        placeholder="Enter email"
+      />
+      <input
+        v-model="password"
+        type="password"
+        name=""
+        id=""
+        placeholder="Enter password"
+      />
+      <button>Sign up</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      // Here, you can perform authentication logic
+      console.log("Username:", this.username);
+      console.log("Password:", this.password);
+    },
+  },
+};
+</script>
+<style>
+body {
+  margin: 0;
+}
+.container {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+form {
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  box-sizing: border-box;
+  border: 1px solid black;
+  padding: 20px;
+}
+form,
+input,
+button {
+  border-radius: 10px;
+}
+input,
+button {
+  padding: 10px;
+}
+</style>
